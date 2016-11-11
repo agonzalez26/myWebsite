a3944cae60698fe522f6744958651051bedf4068
agonzalez26 <agonzalez26@student.gsu.edu>
2016-11-11 11:40:47 -0500

    .

--- footer.php
+++
@@ -1,5 +0,0 @@
-<?php
-  echo "<body>";
-  echo "<footer>Alma Gonzalez 2016</footer>";
-  echo "</body>";
- ?>
--- header.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<body><ul><li><a href="contact.php">Contact</a></li>
-<li><a href="https://drive.google.com/open?id=0B-Rd24-D2c7Zb2JKd1hjbDI0ZFU">Resume</a><li>
-<li><a href="about.php">About</a></li>
-<li><a href="index.php">Home</a></li></ul></body>';
-?>
