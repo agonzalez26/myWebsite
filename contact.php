2dfc1270d82dbbed59e6bb77c4eb6ef09482521c
agonzalez26 <agonzalez26@student.gsu.edu>
2016-11-11 11:29:38 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,6 +5,13 @@
     <title>Contact Me</title>
   </head>
   <body>
+     <div id="top">
+          <div id="icons" style="display:inline;"><ul class="social-icons">
+           <li id="icon icon"class="icon icon--facebook"><a href="https://www.facebook.com/alma.gonzalez.370177"><img src="facebook.png"></a></li>
+           <li id="icon icon"class="icon icon--linkedin"><a href="https://www.linkedin.com/in/alma-gonzalez-268713109?trk=nav_responsive_tab_profile"><img src="linkedin.png"></a></li>
+           <li id="icon icon"class="icon icon--instagram"><a href="https://www.instagram.com/almagonzalez24/?hl=en"><img src="instagram.png"></a></li>
+           <li id="icon icon"class="icon icon--github"><a href="https://github.com/agonzalez26"><img src="github.png"></a></li>
+         </ul></div>
     <div id="navigation">
       <ul>
         <li id="links"><a href="contact.html">Contact</a></li>
